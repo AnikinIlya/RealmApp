1a2abd20ff9de03078c29afe0fb4be31ff104095
AnikinIlya <98335973+AnikinIlya@users.noreply.github.com>
2022-10-13 16:30:08 +0400

    Started RealmApp 1. Created UI 2. Created TaskListViewController and TaskViewController 3. Created TaskList model

---
+++ RealmApp/Models/TaskList.swift
@@ -0,0 +1,22 @@
+//
+//  TaskList.swift
+//  RealmApp
+//
+//  Created by Anikin Ilya on 13.10.2022.
+//
+
+import Foundation
+import RealmSwift
+
+class TaskList: Object {
+    @Persisted var name = ""
+    @Persisted var date = Date()
+    @Persisted var tasks = List<Task>()
+}
+
+class Task: Object {
+    @Persisted var name = ""
+    @Persisted var note = ""
+    @Persisted var date = Date()
+    @Persisted var isComplete = false
+}
